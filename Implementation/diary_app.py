4ee032f0b1c53de5a5a9bda31be8b1fb109db562
sihle <siskosa024jhb@student.wethinkcode.co.za>
2026-02-06 14:38:38 +0200

    added a view entry method

--- Implementation/diary_app.py
+++ Implementation/diary_app.py
@@ -1,9 +1,18 @@
 class Diary:
     def __init__(self):
-        self.create = []
+        self.entries = []
 
     def create_entry(self):
         entry_date = input("Enter the date: ")
         content = input("Enter the content of that day: ")
-        self.create.append({"date": entry_date, "content": content})
+        self.entries.append({"date": entry_date, "content": content})
         print("entry has been created")
+
+    def view_entry(self):
+        if not self.entries:
+            print("No entry found")
+        else:
+            print("Available entries")
+            for entry in self.entries:
+                print(f"Date: {entry['date']}")
+
